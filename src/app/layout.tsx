a2e3eb653a737633349d38eef1cfbd5d5f36a8bb
enunsnv <seoej0315@naver.com>
2024-06-29 19:00:45 +0900

    🔀 merge : pr #2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "aecofarm",
   description: "'아코팜': 대학생 물품 공유 플랫폼",
+  icons: {
+		icon: "/img/hand-logo.svg",
+	},
 };
 
 export default function RootLayout({
--- src/app/lend/page.tsx
+++ src/app/lend/page.tsx
@@ -1,7 +1,92 @@
-import React from "react";
+'use client'
+import styled from 'styled-components';
+import AppLayout from "@/components/layout/MobileLayout";
+import OrangeButton from '@/components/OrangeButton';
+import { useRouter } from 'next/navigation';
 
-const Example = () =>{
-  return(<>Example 페이지로 이동 성공</>)
-}
+const ButtonContainer = styled.div`
+  display: flex;
+  font-size: 14px;
+  margin-top: 10px;
+`;
 
-export default Example;
+const SortButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 22px;
+  color: black;
+  font-size: 15px; 
+
+`;
+
+const SortButton = styled.select`
+  margin: 0 10px;
+  color: black;
+  border: 0px;
+  background-color: white;
+  font-size: 15px; 
+  text-align: center;
+  background-color: white;
+
+  option {
+    background: white;
+    font-size: 15px; 
+  }
+`;
+
+
+const DonateContainer = styled.div`
+  margin-left: 110px;
+  width: 110px;
+  position: relative;
+`;
+
+const CheckDonateButton = styled.button`
+  margin: 0 5px;
+  color: black;
+  border: 0px;
+  background-color: white;
+  width: auto;
+`;
+
+const CheckIcon = styled.img`
+  position: absolute;
+  width: 18px;
+  right: 10px; /* 필요에 따라 조정 */
+  top: 50%; /* 아이콘을 수직 중앙에 위치 */
+  transform: translateY(-50%);
+`;
+
+const SignUpPage = () => {
+  const router = useRouter();
+
+  const login = () => {
+    router.push('/lend');
+  };
+
+  const handleClick = () => {
+    router.push('/sign-up');
+  };
+
+  return (
+    <AppLayout>
+      <ButtonContainer>
+        <SortButtonContainer>
+          <SortButton>
+            <option key="최신순" value="latest">최신순</option>
+            <option key="거리순" value="distance">거리순</option>
+            <option key="낮은 가격순" value="low-price">낮은 가격순</option>
+            <option key="높은 가격순" value="high-price">높은 가격순</option>
+          </SortButton>
+        </SortButtonContainer>
+
+        <DonateContainer>
+          <CheckDonateButton>기부 모아보기</CheckDonateButton>
+          <CheckIcon src='/img/not-checked.svg' alt='check' />
+        </DonateContainer>
+      </ButtonContainer>  
+    </AppLayout>
+  );
+};
+
+export default SignUpPage;
