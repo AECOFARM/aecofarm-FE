a9d0e13e67970637c5d3fd8c86093c236e4db249
jungsun <dlwjdtjs0111@gmail.com>
2024-07-01 15:27:15 +0900

    ✨ Feat: 상품 결제하기 페이지 추가

---
+++ src/app/pay/layout.tsx
@@ -0,0 +1,29 @@
+"use client"
+import Header from '@/components/Header';
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Navigation from '@/components/Navigation';
+import MainLayout from '@/components/layout/MainLayout';
+import AppLayout from '@/components/layout/MobileLayout';
+import TopBar from '@/components/TopBar';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>
+        <AppLayout>
+          <Header />
+            <TopBar text = "상품 결제하기" />
+              {children}
+          <Navigation />
+        </AppLayout>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/pay/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from "react";
+import styled from 'styled-components';
+import TopBar from "@/components/TopBar";
+
+const Pay = () => {
+  return (
+      <TopBar text="결제하기" />
+  );
+};
+
+export default Pay;
