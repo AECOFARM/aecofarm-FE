3f1cd0d6475fc2c3143c203dcf10d291933f3f31
jungsun <dlwjdtjs0111@gmail.com>
2024-07-03 23:57:52 +0900

    ♻️ Refactor: 마이페이지 코드 리펙토링

---
+++ src/components/MyItemListItem.tsx
@@ -0,0 +1,109 @@
+import styled from 'styled-components';
+import { NextPage } from 'next';
+import React, {useState} from 'react';
+
+const ItemContainer = styled.div`
+  .profileImage {
+    border-radius: 10px;
+    width: 7rem;
+    height: 7rem; 
+  }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  padding: 5px;
+  background-color: #FFFFFF;
+  border-radius: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #EBEBEB;
+  }
+  position: relative;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 20px;
+  }
+  position: absolute;
+  top: 5.8rem;
+  left: 5.8rem;
+`;
+
+const ItemInfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+  p {
+    color: #686868;
+    font-size: 0.8rem;
+    font-weight: 700;
+  }
+  .time {
+    color: #000000;
+  }
+  .price {
+    color: #DF5532;
+  }
+`;
+
+const ItemTitle = styled.p`
+  font-size: 0.8rem;
+  font-weight: 700;
+  color: #686868;
+`;
+
+interface Item {
+  contractId: number;
+  itemName: string;
+  image: string;
+  time: number;
+  price: number;
+  likeStatus: boolean;
+}
+
+interface ItemProps {
+  item: Item;
+}
+
+const MyItemListItem: React.FC<ItemProps> = ({ item }) => {
+  const {
+    contractId,
+    itemName,
+    image,
+    time,
+    price,
+    likeStatus: initialLikeStatus,
+  } = item;
+
+  const [likeStatus, setLikeStatus] = useState(initialLikeStatus);
+  
+  const likeIconSrc = likeStatus ? '/img/red-heart.svg' : '/img/empty-heart.svg';
+
+  const toggleLikeStatus = () => {
+    setLikeStatus(prevStatus => !prevStatus);
+  }
+
+  return (
+    <ItemContainer>
+      <img className='profileImage' src = {image} alt = "item" />
+      <ItemInfoContainer>
+        <p className='price'>{price}</p>
+        <p>|</p>
+        <p className='time'>{time}시간 대여</p>
+      </ItemInfoContainer>
+      <ItemTitle>{itemName}</ItemTitle>
+      <IconContainer>
+        <img src = {likeIconSrc} alt = "like" onClick={toggleLikeStatus} />
+      </IconContainer>
+    </ItemContainer>
+  )
+}
+
+export default MyItemListItem;
+  
