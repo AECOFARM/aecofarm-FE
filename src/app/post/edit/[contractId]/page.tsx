fe380d3d4d976e74c63d4b69e2d880993305fed7
jungsun <dlwjdtjs0111@gmail.com>
2024-07-17 18:10:06 +0900

    💄 Style: post input 스타일 수정

--- src/app/post/edit/[contractId]/page.tsx
+++ src/app/post/edit/[contractId]/page.tsx
@@ -8,117 +8,103 @@ import MainLayout from "@/components/layout/MainLayout";
 import { Wrapper } from "@/components/CommonStyles";
 import TopBar from "@/components/TopBar";
 import PostButton from "../../components/PostButton";
+import axios from "axios";
 
 interface ItemDetail {
-    category: string,
-    contractId: number;
+    owner: boolean;
     userName: string;
     itemName: string;
-    itemContents: string;
-    kakao: string;
-    itemImage: string;
     price: number;
+    itemImage?: string;
+    itemContents: string;
     itemPlace: string;
+    itemHash: string[];
     time: number;
     contractTime: number;
-    itemHash: string[];
-    likeStatus: boolean;
-    donateStatus: boolean;
+    kakao: string;
+    category?: string;
+    file?: File;
   }
 
 const exampleData: ItemDetail[] = [
-    {
-        "category" : "BORROW",
-        "contractId": 123456,
-        "userName": "이정선",
-        "itemName": "맥북 맥세이프 충전기",
-        "itemContents": "상태 최상. 아이폰, 갤럭시 동시에 충전 가능!",
-        "kakao": "https://open.kakao.com/o/s37YOrBg",
-        "itemImage": "",
-        "price": 0,
-        "itemPlace": "경영관",
-        "time": 5,
-        "contractTime": 10,
-        "itemHash": ["eunjeong", "맥북프로", "충전기"],
-        "likeStatus": true,
-        "donateStatus": true
-    },
-    {
-        "category" : "BORROW",
-        "contractId": 789012,
-        "userName": "이정선",
-        "itemName": "아이패드 에어 4",
-        "itemImage": "/img/item-image.png",
-        "itemContents": "상태 최상. 아이폰, 갤럭시 동시에 충전 가능!",
-        "kakao": "https://open.kakao.com/o/s37YOrBg",
-        "price": 5000,
-        "itemPlace": "신공학관",
-        "time": 3,
-        "contractTime": 10,
-        "itemHash": ["jeongseon", "네고가능", "상태좋음"],
-        "likeStatus": false,
-        "donateStatus": false,
-    },
-    {
-        "category" : "BORROW",
-        "contractId": 789013,
-        "userName": "이정선",
-        "itemName": "아이패드 에어 4",
-        "itemImage": "/img/item-image.png",
-        "itemContents": "상태 최상. 아이폰, 갤럭시 동시에 충전 가능!",
-        "kakao": "https://open.kakao.com/o/s37YOrBg",
-        "price": 5000,
-        "itemPlace": "신공학관",
-        "time": 3,
-        "contractTime": 10,
-        "itemHash": ["jeongseon", "네고가능", "상태좋음"],
-        "likeStatus": false,
-        "donateStatus": false,
-    },
-    {
-        "category" : "BORROW",
-        "contractId": 789014,
-        "userName": "이정선",
-        "itemName": "아이패드 에어 4",
-        "itemImage": "/img/item-image.png",
-        "itemContents": "상태 최상. 아이폰, 갤럭시 동시에 충전 가능!",
-        "kakao": "https://open.kakao.com/o/s37YOrBg",
-        "price": 0,
-        "itemPlace": "신공학관",
-        "time": 3,
-        "contractTime": 10,
-        "itemHash": ["jeongseon", "네고가능", "상태좋음"],
-        "likeStatus": false,
-        "donateStatus": true,
-    },
-    {
-        "category" : "BORROW",
-        "contractId": 789015,
-        "userName": "이정선",
-        "itemName": "아이패드 에어 4",
-        "itemContents": "상태 최상. 아이폰, 갤럭시 동시에 충전 가능!",
-        "kakao": "https://open.kakao.com/o/s37YOrBg",
-        "itemImage": "",
-        "price": 0,
-        "itemPlace": "신공학관",
-        "time": 3,
-        "contractTime": 10,
-        "itemHash": ["jeongseon", "네고가능", "상태좋음"],
-        "likeStatus": false,
-        "donateStatus": true,
-    }
+    
   ];
 
 const UpdatePost = () => {
 
+
   const router = useRouter();
 
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
   const { contractId } = useParams() as { contractId: string };
-  const [itemDetail, setItemDetail] = useState<ItemDetail | null>(null);
+  const [itemDetail, setItemDetail] = useState<ItemDetail>({
+    owner: false,
+    userName: "",
+    itemName: "",
+    price: 0,
+    itemContents: "",
+    itemPlace: "",
+    itemHash: [],
+    time: 0,
+    contractTime: 0,
+    kakao: "",
+    itemImage: "",
+    file: undefined,
+  });
   const [category, setCategory] = useState<string | null>(null);
   const [tags, setTags] = useState<{ value: string }[]>([]);
   const [value, setValue] = useState("");
   const [hasHashWrapInner, setHasHashWrapInner] = useState(false);
+
+  const updatePost = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const updateContract = JSON.stringify({
+      category: itemDetail?.category,
+      itemName: itemDetail?.itemName,
+      kakao: itemDetail?.kakao,
+      itemHash: itemDetail?.itemHash,
+      time: itemDetail?.time,
+      contractTime: itemDetail?.contractTime,
+      price: itemDetail?.price,
+      itemPlace: itemDetail?.itemPlace,
+      itemContents: itemDetail?.itemContents
+    });
+
+    const blob = new Blob([updateContract], {
+      type: 'application/json'
+    });
+
+    const formData = new FormData();
+    formData.append('updateContract', blob);
+
+    if (itemDetail?.file) {
+      formData.append('file', itemDetail.file);
+    } else {
+      const emptyFile = new File([""], "empty.txt", {type: "text/plain"});
+      formData.append('file', emptyFile);
+      // formData.append('file', ''); 로 수정
+    }
+
+    try {
+      setError(null);
+      setLoading(true);
+      const token = localStorage.getItem('token');
+      const response = await axios.put(`https://port-0-aecofarm-lyhj20nc49bb1c32.sel5.cloudtype.app/contract/update/${contractId}`, formData, {
+        headers : {
+          'Content-Type': 'multipart/form-data',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      console.log(response);
+      router.push("/borrow"); // 디테일 페이지로 수정
+    } catch(err) {
+      setError(err.message || 'Something went wrong');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     const hashWrapInnerElements = document.querySelectorAll(".HashWrapInner");
@@ -128,26 +114,42 @@ const UpdatePost = () => {
   const handleTagsChange = (e: CustomEvent) => {
     setTags(e.detail.value);
   };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const {name, value} = e.target;
+    setItemDetail(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
   
   useEffect(() => {
     if(!contractId)
       return;
     
-    const fetchItemInfo = () => {
-      const currentItem = exampleData.find(
-        (item) => item.contractId === Number(contractId)
-      );
-      setItemDetail(currentItem || null);
-      setCategory(currentItem?.category || null);
-      if (currentItem) {
-        setTags(currentItem.itemHash.map((tag) => ({ value: tag })));
+    const fetchItemInfo = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const responce = await axios.get(`https://port-0-aecofarm-lyhj20nc49bb1c32.sel5.cloudtype.app/contract/detail/${contractId}`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        const currentItem = responce.data;
+        setItemDetail(currentItem);
+        setCategory(currentItem?.category || null);
+        if (currentItem.itemHash) {
+          setTags(currentItem.itemHash.map((tag: string) => ({ value: tag })));
+        }
+      } catch (err) {
+        setError(err.message || 'Something went wrong');
       }
     };
     fetchItemInfo();
   }, [contractId]);
 
-  if (!itemDetail) return <div>Loading...</div>;
-  
+  if (!itemDetail) return <div>contractId를 확인하세요</div>;
+
   return(
     <MainLayout>
       <TopBar text="글 수정하기" />
@@ -159,7 +161,7 @@ const UpdatePost = () => {
             <p>빌리고 싶어요</p>
            )}
         </Category>
-        <InputContainer>
+        <InputContainer onSubmit={updatePost}>
         {category === "BORROW" && (
           <ImageInputContainer>
             <div className="image" />
@@ -169,46 +171,45 @@ const UpdatePost = () => {
           placeholder="상품명"
           name="itemName"
           value={itemDetail.itemName}
-          onChange={(e) => setItemDetail(e.target.value)}
+          onChange={handleInputChange}
         />
         <TextInput
           placeholder="오픈채팅방 링크"
           name="kakao"
           value={itemDetail.kakao}
-          onChange={(e) => setItemDetail(e.target.value)}
+          onChange={handleInputChange}
         />
-
-        <TagInput value={tags} onChange={handleTagsChange} placeholder="해시태그 입력" />
+        <TagInput value={itemDetail.itemHash} onChange={handleTagsChange} placeholder="해시태그 입력" />
         <TextInput
           placeholder="가격"
           name="price"
-          value={itemDetail.price}
-          onChange={(e) => setItemDetail(e.target.value)}
+          value={String(itemDetail.price)}
+          onChange={handleInputChange}
         />
         <TextInput
           placeholder="거래 가능 장소"
           name="itemPlace"
           value={itemDetail.itemPlace}
-          onChange={(e) => setItemDetail(e.target.value)}
+          onChange={handleInputChange}
         />
         <TextInput
           placeholder="거래 가능 시간"
-          name="time"
-          value={itemDetail.time}
-          onChange={(e) => setItemDetail(e.target.value)}
+          name="contractTime"
+          value={String(itemDetail.contractTime)}
+          onChange={handleInputChange}
         />
         <TextInput
           placeholder="대여 가능 시간"
           name="contractTime"
-          value={itemDetail.contractTime}
-          onChange={(e) => setItemDetail(e.target.value)}
+          value={String(itemDetail.time)}
+          onChange={handleInputChange}
         />
         <ItemInfoContainer>
           <p>설명</p>
-          <textarea placeholder="상품의 상태를 자세히 적어주세요." defaultValue={itemDetail.itemContents}/>
+          <textarea placeholder="상품의 상태를 자세히 적어주세요." value={itemDetail?.itemContents} name="itemContents" onChange={handleInputChange}/>
         </ItemInfoContainer>
         </InputContainer>
-      <PostButton text="수정하기"/>
+      <PostButton text="수정하기" onClick={updatePost}/>
       </Wrapper>
     </MainLayout>
   );
@@ -226,7 +227,7 @@ const Category = styled.div`
   align-items: center;
 `;
 
-const InputContainer = styled.div`
+const InputContainer = styled.form`
   width: 100%;
   padding: 25px;
   display: flex;
