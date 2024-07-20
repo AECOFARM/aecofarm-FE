100297bcdcad18e088c44d14dc92733fd2f2667a
enunsnv <seoej0315@naver.com>
2024-07-21 03:16:03 +0900

    🐛 fix: 기부하기 상태 타입 명시

--- src/app/borrow-detail/components/SeeDonate.tsx
+++ src/app/borrow-detail/components/SeeDonate.tsx
@@ -22,24 +22,27 @@ const CheckIcon = styled.img`
   padding: 0 2px;
 `;
 
-const SeeDonate = () => {
+// 타입 명시적으로 지정
+interface SeeDonateProps {
+  setSeeDonateStatus: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-  const [seeDonateStatus, setSeeDonateStatus] = useState(false);
+const SeeDonate: React.FC<SeeDonateProps> = ({ setSeeDonateStatus }) => {
+  const [seeDonateStatus, setLocalSeeDonateStatus] = useState(false);
 
   const SeeDonateSrc = seeDonateStatus ? '/img/not-checked.svg' : '/img/donate-check.svg';
 
   const checkSeeDonateStatus = () => {
-    setSeeDonateStatus(prevStatus => !prevStatus);
+    setLocalSeeDonateStatus(prevStatus => !prevStatus);
+    setSeeDonateStatus(prevStatus => !prevStatus); // 부모 컴포넌트의 상태 업데이트
   };
-
 
   return (
     <DonateContainer onClick={checkSeeDonateStatus}>
       <CheckDonateButton>기부 모아보기</CheckDonateButton>
       <CheckIcon src={SeeDonateSrc} alt='check' />
     </DonateContainer>
-  )
-  
+  );
 };
 
 export default SeeDonate;
