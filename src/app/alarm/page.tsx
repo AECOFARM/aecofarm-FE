b4f09b0176a93369b94cf759ab1236bab5caa302
enunsnv <seoej0315@naver.com>
2024-07-23 12:32:21 +0900

    💚 fix: alarm props 타입 지정

--- src/app/alarm/page.tsx
+++ src/app/alarm/page.tsx
@@ -1,34 +1,12 @@
 'use client'
-import React, {useState, useEffect} from "react"
-import styled from "styled-components"
+import React from "react";
 import { Wrapper } from "@/components/CommonStyles";
 import AlarmList from "./components/AlarmList";
-import axios from "axios";
 
-const Alarm = () => {
-    const [alarmList, setAlarmList] = useState({ lending: [], borrowing: [] });
-
-    useEffect(() => {
-      const fetchAlarm = async() => {
-        try {
-          const token = localStorage.getItem('token');
-          const response = await axios.get('/api/alarm/list', {
-            headers: {
-              'Authorization': `Bearer ${token}`
-            }
-          });
-          const data = response.data.data;
-          setAlarmList(data);
-        } catch (err) {
-          console.error(err.message || 'Something went wrong');
-        }
-      };
-      fetchAlarm();
-    }, []);
-
+const Alarm: React.FC = () => {
     return (
         <Wrapper>
-            <AlarmList alarmList={alarmList} />
+            <AlarmList />
         </Wrapper>
     );
 }
