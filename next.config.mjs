6befa42aafe8622c41cb3c1f3e5e4d49f7ed3e68
enunsnv <seoej0315@naver.com>
2024-07-25 04:53:54 +0900

    🐛 fix: next config 경로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/usports/:path*',
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
