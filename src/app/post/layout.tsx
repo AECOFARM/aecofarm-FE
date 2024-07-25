34ec3adb320fe8ba3edbf2669c3a1f3390a3fe7d
enunsnv <seoej0315@naver.com>
2024-07-25 16:22:29 +0900

    🔀 merge: post dir

---
+++ src/app/post/layout.tsx
@@ -0,0 +1,21 @@
+"use client"
+import Header from '@/components/Header';
+import { Inter } from "next/font/google";
+import "../globals.css";
+import AppLayout from '@/components/layout/MobileLayout';
+import TopBar from '@/components/TopBar';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+        <AppLayout>
+          <Header />
+          {children}
+        </AppLayout>
+  );
+}
